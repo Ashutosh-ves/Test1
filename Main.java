474405fc2bc1445f1b68587f7d154a6da89ef36d
Ashutosh-ves <2024.ashutosh.mishra@ves.ac.in>
2025-09-17 10:09:22 +0530

    Update 2 Main.java

--- Main.java
+++ Main.java
@@ -1,8 +1,14 @@
-import java.util.*;
-public class Main{
-    public static void main(String args[]){
-        System.out.println("Hello World!");
-        System.out.println("Second change");
-    }
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the first integer: ");
+        int num1 = scanner.nextInt();
+        System.out.print("Enter the second integer: ");
+        int num2 = scanner.nextInt();
+        int sum = num1 + num2;
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+        scanner.close();
+    }
 }
